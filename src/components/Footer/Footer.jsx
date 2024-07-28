291e2ade8b6b3d4a95ec9e1c2c95a7d8efc39259
Rayhan Baee <aboraihan14@gmail.com>
2024-07-28 17:52:13 +0600

    add something new

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,15 @@ function Footer() {
     <div>
         <section>
 
-            <div></div>
+            <div>
+                <h1>Get In Touch</h1>
+                <p>Dit amet consectetur. Condimentum  adipiscing aliquam turpis placerat dolor. Purus urna in sit nullam proin. </p>
+
+                <p>hockeys@support.com</p>
+                <p>(+62) 123-321-543</p>
+            </div>
+
+
             <div></div>
             <div></div>
             <div></div>
