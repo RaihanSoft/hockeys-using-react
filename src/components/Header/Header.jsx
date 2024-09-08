b59f3e5edb2a8cf5079333d916d951f3220bde10
Rayhan Baee <aboraihan14@gmail.com>
2024-09-08 17:18:05 +0600

    banner content add and responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,23 @@
 function Header() {
   return (
-    <div className="">
+    <div>
+      <section className="w-11/12 mx-auto xl:relative mt-5">
+
+      <div className="xl:h-[550px] mx-auto">
+        <img className="mx-auto" src='./Images/1.png' alt="" />
+      </div>
+
+      <div className="xl:absolute xl:bottom-5 xl:right-36 bg-black text-white p-5 rounded-xl">
+      <h1 className="text-2xl font-bold">Meet all the heroes from the field</h1>
+      <p>Dit amet consectetur. Condimentum dignissim adipiscing aliquam turpis <br /> placerat dolor. Purus urna in sit nullam proin. </p>
+      </div>
+
+      {/* arrow  */}
+      
 
 
 
+      </section>
     </div>
   );
 }
