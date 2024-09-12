1ff232d1422afab2eccf5589f5b5bf575f07ba5c
Rayhan Baee <aboraihan14@gmail.com>
2024-09-12 17:16:13 +0600

    client section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Client from "./components/Client/Client";
 import Club from "./components/Club/Club";
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
@@ -13,6 +14,7 @@ function App() {
      <Club></Club>
      <Program></Program>
      <Product></Product>
+     <Client></Client>
     </div>
   );
 }
---
+++ src/components/Client/Client.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+
+function Client() {
+  return (
+    <div>
+
+        <section className='flex'>
+            <div>
+                <img src="./Images/11.png" alt="" />
+
+            </div>
+
+
+
+
+            <div>
+
+            <div className="collapse collapse-plus bg-base-200">
+  <input type="radio" name="my-accordion-3" defaultChecked />
+  <div className="collapse-title text-xl font-medium">Click to open this one and close others</div>
+  <div className="collapse-content">
+    <p>hello</p>
+  </div>
+</div>
+<div className="collapse collapse-plus bg-base-200">
+  <input type="radio" name="my-accordion-3" />
+  <div className="collapse-title text-xl font-medium">Click to open this one and close others</div>
+  <div className="collapse-content">
+    <p>hello</p>
+  </div>
+</div>
+<div className="collapse collapse-plus bg-base-200">
+  <input type="radio" name="my-accordion-3" />
+  <div className="collapse-title text-xl font-medium">Click to open this one and close others</div>
+  <div className="collapse-content">
+    <p>hello</p>
+  </div>
+</div>
+<div className="collapse collapse-plus bg-base-200">
+  <input type="radio" name="my-accordion-3" />
+  <div className="collapse-title text-xl font-medium">Click to open this one and close others</div>
+  <div className="collapse-content">
+    <p>hello</p>
+  </div>
+</div>
+<div className="collapse collapse-plus bg-base-200">
+  <input type="radio" name="my-accordion-3" />
+  <div className="collapse-title text-xl font-medium">Click to open this one and close others</div>
+  <div className="collapse-content">
+    <p>hello</p>
+  </div>
+</div>
+<div className="collapse collapse-plus bg-base-200">
+  <input type="radio" name="my-accordion-3" />
+  <div className="collapse-title text-xl font-medium">Click to open this one and close others</div>
+  <div className="collapse-content">
+    <p>hello</p>
+  </div>
+</div>
+
+
+
+            </div>
+        </section>
+
+
+
+
+    </div>
+  )
+}
+
+export default Client
